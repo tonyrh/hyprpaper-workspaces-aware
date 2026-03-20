5bd5ea347f02f20968377b701d83e45d6d583103
tonyrh <antonio.giungato@gmail.com>
2026-03-20 11:55:53 +0100

    Upload script

---
+++ wallhp.zsh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+echo $(date): $XDG_RUNTIME_DIR - ${HYPRLAND_INSTANCE_SIGNATURE} >> /home/antonio/output.log
+
+function handle {
+    if [[ ${1:0:11} == "workspace>>" ]]; then
+	hyprctl hyprpaper wallpaper "DP-2,/home/antonio/Pictures/Wallpaper/"${line:0-1}".jpg"
+	echo "Changing wallpaper" >> /home/antonio/output.log
+    fi
+}
+
+socat - UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/${HYPRLAND_INSTANCE_SIGNATURE}/.socket2.sock >> /home/antonio/output.log 2>&1 | while read line; do handle $line; done
